fd00deddca26f5594b6b6b1db82cd2617d728e1e
symas <tk@symas.ch>
2026-08-24 12:53:32 +0200

    MB02

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ClassDemo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Rechteck r1 = new Rechteck();
+            r1.Hoehe = 1;
+            r1.Breite = 2;
+            r1.Position = new Punkt() { X = 3, Y = 3 };
+            r1.LinienFarbe = "grün";
+            //r1.flaeche = 7;
+
+            //r1.Zeichne();
+            //r1.Skaliere(0.5);
+            //r1.Zeichne();
+            //r1.Verschiebe(2,3);
+            //r1.Skaliere(0.5);
+            //r1.Zeichne();
+
+            Rechteck r2 = new Rechteck();
+            r2.Hoehe = 9;
+            r2.Breite = 8;
+            r2.LinienFarbe = "blau";
+            r2.Position = new Punkt() { Y = 4, X = 5 };
+            //Rechteck.Winkelsumme = 400;
+            r2.Zeichne();
+            r1.Zeichne();
+        }
+    }
+
+    public class Rechteck {
+
+        //public int Id;  // Klassenvariable oder Attribut
+        public readonly int Id;  // readonly Klassenvariable oder Attribut 
+        public double Hoehe { get; set; }   // Property
+        //public double Breite { get; }   // ReadOnly-Property
+        public double Breite { get; set;  }   // ReadOnly-Property
+
+        public double Umfang {
+            get { 
+                //return Hoehe + Breite + Hoehe + Breite;
+                return 2 * Hoehe + 2 * Breite;
+            }
+        }
+
+        public Punkt Position { get; set; }
+
+        public string LinienFarbe { get; set; }
+
+        public static int Winkelsumme { get;  } = 360;
+
+        private double flaeche;
+
+        public void Verschiebe(double dx, double dy) {
+            double newX = Position.X + dx;
+            double newY = Position.Y + dy;
+
+            Position = new Punkt() { X = newX, Y = newY };
+        }
+
+        public void Skaliere(double faktor) {
+            Breite = Breite * faktor;
+            Hoehe = Hoehe * faktor;
+        }
+
+        public void Zeichne() {
+            Console.WriteLine($"Rechteck Id={Id}, Hoehe={Hoehe}, Breite={Breite}, Winkelsumme={Winkelsumme}, Umfange={Umfang}, Position.X={Position.X}, Position.Y={Position.Y}, LinienFarbe={LinienFarbe}");
+        }
+    }
+
+    public class Punkt {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ RepArray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RepArray {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Frage an den User: wie gross soll der Int-Array sein?
+            Console.WriteLine("Wie gross soll der Int-Array sein?");
+            string strSize = Console.ReadLine();
+
+            int size = int.Parse(strSize);
+
+            int[] a = new int[size];
+            //int[] a = new int[] { 200, 450, 700, 175, 250 };
+
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++) {
+                sum = sum + a[i];
+            }
+
+            Console.WriteLine(sum);
+
+        }
+
+        //Console.WriteLine("aaa");
+        int version;
+    }
+}
